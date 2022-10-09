3d97936c1e9b9bd123ffbb87612a4aa00bd4b4aa
waltermachinena <waltermachinena@gmail.com>
2022-10-09 01:07:18 -0500

    Se agregan plantillas

--- Controlador/conexion.php
+++ Controlador/conexion.php
@@ -1,43 +1,21 @@
 <?php
-    include 'conexion.php';
-    // session_start();
-?>
+    $server = "localhost";
+    $user = "siecymel_Kilzms234";
+    $password ="L4742#3157_L";
+    $dbname = "siecymel_funcInternasSiec";
 
-<?php
+    try {
+        // Conectar la BD
+        $dbConexion = new PDO("mysql:host=$server;dbname=$dbname", 
+        $user, $password);
+        //Nivel de errores a excepcion
+        $dbConexion ->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 
-    $user = $_POST['usuario'];
-    $password = $_POST['password'];
+        // Que acepte caracteres especiales latinos - españoles
+        $dbConexion->exec("SET CHARACTER SET utf8");
 
-    // Validaciones para que no realicen inyeccion de codigo
-
-    // Query para validar los datos
-    $query = "SELECT idUsuario, NombreUsuario, Contrasena, idTipoUsuario, numEmpleado FROM Usuario 
-        WHERE NombreUsuario = :idUsuario
-        AND Contrasena =:contrasena";
-
-    // Se prepara la sentencia
-    $resultado=$dbConexion->prepare($query);
-
-    // Se ejecuta la sentencia en forma de array
-    $resultado->execute(array(":idUsuario"=>$user, ":contrasena"=>$password));
-
-
-    // Verifica que el usuario existe -  muestra el mensaje si existe o no.
-    if ($registro=$resultado->fetch(PDO::FETCH_ASSOC)) {
-            echo "Bienvenido";
-        
-        echo "Bienvenido: " ;
-
-        if ($user == "FcoLerma1") { // Unicamente si el usuario es 0, se creara la sesion de administrador.
-            $_SESSION['usuarioAdmin']="A";
-        } else { // Si el usuario no es 0000, se crea la variable usuarioNormal o tambien conocido como Cliente.
-            $_SESSION['usuarioNormal'] = "B";
-        }
-        
-        echo "<a href='../Vista/login.html'> Regresar al login </a>";
-    } else {
-        echo "<script>alert('Los datos ingresados son incorrectos');</script>";
-        echo "<a href='/index.html'> Regresar</a>";
+    } catch(PDOException $e) {
+        echo "Error: " .$e->getMessage();
     }
-    $resultado->closeCursor();
+
 ?>
