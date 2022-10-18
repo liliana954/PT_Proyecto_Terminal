fb3fcfc88cd935375146b8039ecee69e9ec4223b
waltermachinena <waltermachinena@gmail.com>
2022-10-17 19:05:47 -0500

    se modifica texto

--- Config/conexion.php
+++ Config/conexion.php
@@ -1,16 +1,14 @@
 <?php
-    $server = "localhost";
-    $user = "siecymel_Kilzms234";
-    $password ="L4742#3157_L";
-    $dbname = "siecymel_funcInternasSiec";
+    $server = "localhost";  // siempre va localhost
+    $user = "siecymel_Kilzms234"; // usuario hosting
+    $password ="L4742#3157_L"; // contraseña hosting
+    $dbname = "siecymel_funcInternasSiec";  // nombre BD
 
     try {
         // Conectar la BD
-        $dbConexion = new PDO("mysql:host=$server;dbname=$dbname", 
-        $user, $password);
+        $dbConexion = new PDO("mysql:host=$server;dbname=$dbname",$user, $password);
         //Nivel de errores a excepcion
         $dbConexion ->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-
         // Que acepte caracteres especiales latinos - españoles
         $dbConexion->exec("SET CHARACTER SET utf8");
 
